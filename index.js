a5a08f713ef427f7cfbf5c0144c9a76eca1c557f
Adnan Asani <konjuku@gmail.com>
2018-04-02 23:52:02 +0200

    feat(styled): add styled-components

---
+++ index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ResultItem = styled.li`
+  color: red;
+`;
+
+const ResultList = styled.ul`
+  outline: 1px solid blue;
+`;
+
+export { ResultItem, ResultList };
